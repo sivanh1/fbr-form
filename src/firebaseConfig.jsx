6a5e509b3a66aa316cb0aa472e8bb17b5b575459
Sivanesh P <154664183+sivanh1@users.noreply.github.com>
2025-03-22 14:33:56 +0530

    Update firebaseConfig.jsx

--- src/firebaseConfig.jsx
+++ src/firebaseConfig.jsx
@@ -3,12 +3,6 @@ import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyB3QOHzy_pRG6yK0vTP7IhJjOXJGnZd_Fs",
-    authDomain: "form-99e4f.firebaseapp.com",
-    projectId: "form-99e4f",
-    storageBucket: "form-99e4f.firebasestorage.app",
-    messagingSenderId: "369879142751",
-    appId: "1:369879142751:web:b0c0480ec53a92d4c366ea"
   };
 
 // Initialize Firebase
